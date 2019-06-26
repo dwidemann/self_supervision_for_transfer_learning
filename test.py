474d5208e41b456e8c7066f2031fb7dddb41b0be
widemann1 <widemann@llnl.gov>
2019-06-26 18:39:55 +0000

    code for apron dataloader test/train

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def main(config, resume):
     # setup data_loader instances
     data_loader = getattr(module_data, config['data_loader']['type'])(
         config['data_loader']['args']['data_dir'],
-        batch_size=512,
+        batch_size=8,
         shuffle=False,
         validation_split=0.0,
         training=False,
