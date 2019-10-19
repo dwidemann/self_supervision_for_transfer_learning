814580302ed0cd2a35bbb7a48741c50f1ce35011
martinezniev1 <martinezniev1@llnl.gov>
2019-10-18 17:34:15 -0700

    fixed problem where model didn't want to accept the data loaded using the AcousticDataLoader class.

--- utils/generate_outputs_from_ae_model.py
+++ utils/generate_outputs_from_ae_model.py
@@ -5,41 +5,13 @@ sys.path.append(os.path.abspath("base/"))
 sys.path.append(os.path.abspath("."))
 
 import numpy as np
+import matplotlib.pyplot as plt
+from os.path import expanduser
 from model import ResNet_AE
 from data_loaders import AcousticDataset
 import torch
 
-if __name__ == '__main__':
-    
-    model = ResNet_AE()
-
-    state = torch.load('/data/acoustic_tmp/ae_acoustic_results_batch_16/models/acoustic_ae/1016_170641/model_best.pth')
-    model.load_state_dict(state['state_dict'])
-    model.eval()
-
-    #load data
-    dataset = AcousticDataset('/gsceph/adapd/acoustic/AA_10/train.pkl')
-    
-    idxs = np.random.randint(0, len(dataset), 3)
-    data = []
-    for i in idxs:
-        img, label = dataset[i]
-        name = dataset.data[0][0]
-        data.append((name, img, label))
-    data = np.array(data)
-    
-    #generate data
-    imgs = np.vstack(data[:, 1])
-    #print(imgs)
-    outputs = model(torch.from_numpy(imgs))
-
-    #plot inputs and outputs
-    for index, (full_name, img, label) in enumerate(data):
-        name = full_name.split('/')[-1].replace('.pkl', '')
-        
-        save_sample(img.numpy(), '~/acoustic/ae_outputs_batch_16/' + name + '_' + str(label) + '.png')
-        save_sample(outputs[index].numpy(), '~/acoustic/ae_outputs_batch_16/output_' + name + '_' + str(label) + '.png')
-
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 def save_sample(data, outfile):
     fig = plt.figure()
@@ -58,4 +30,44 @@ def save_sample(data, outfile):
     # ax.set_aspect(1.0/ax.get_data_ratio()*ratio)
     plt.savefig(fname=outfile,dpi=300,format='png')
 
+
+
+
+if __name__ == '__main__':
+    home = home = expanduser("~")
+    
+    save_dir = home + '/acoustic/ae_outputs_batch_16/'
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    model = ResNet_AE().to(device)
+    
+    state = torch.load('/data/acoustic_tmp/ae_acoustic_results_batch_16/models/acoustic_ae/1016_170641/model_best.pth')
+    model.load_state_dict(state['state_dict'])
+    model.eval()
+
+    #load data
+    dataset = AcousticDataset('/gsceph/adapd/acoustic/AA_10/train.pkl')
+    
+    idxs = np.random.randint(0, len(dataset), 10)
+    print(idxs)
+    data = []
+    for i in idxs:
+        img, label = dataset[i]
+        name = dataset.data[i][0]
+        data.append((name, img, label))
+    data = np.array(data)
+    print(data[:, 0])
+
+    #generate data
+    imgs = np.expand_dims(np.vstack(data[:, 1]), axis=1)
+    print(imgs.shape)
+    outputs = model(torch.from_numpy(imgs).to(device)).cpu().detach().numpy()
+
+    #plot inputs and outputs
+    for index, (full_name, _, label) in enumerate(data):
+        name = full_name.split('/')[-1].replace('.pkl', '')
+        #print(name)
+        save_sample(np.squeeze(imgs[index], axis=0), save_dir + name + '_' + str(label) + '.png')
+        save_sample(np.squeeze(outputs[index], axis=0), save_dir + 'output_' + name + '_' + str(label) + '.png')
 
